cbcff351820cba4f2c03c40876bce2e416a5650d
Simon Daley-Yates <sddyates@gmail.com>
2021-05-05 12:21:24 +0100

    removed cython imports form algorithm class

--- mars/algorithms.py
+++ mars/algorithms.py
@@ -3,9 +3,9 @@ import sys
 import numpy as np
 
 from riemann_solvers import tvdlf, hll, hllc
-from cython_lib.riemann_solvers import tvdlf_pyx, hll_pyx, hllc_pyx
+# from cython_lib.riemann_solvers import tvdlf_pyx, hll_pyx, hllc_pyx
 from reconstruction import flat, minmod
-from cython_lib.reconstruction import flat_pyx, minmod_pyx
+# from cython_lib.reconstruction import flat_pyx, minmod_pyx
 from time_stepping import Euler, RungaKutta2, RungaKutta3
 
 
